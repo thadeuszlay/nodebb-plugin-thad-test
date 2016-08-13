3bbed6cd30e108825de672ad7461766d8c86ede4
thadeuszlay <thadeuszlay@gmail.com>
2016-08-13 20:40:31 +0200

    modify route and file name to thadTest

--- library.js
+++ library.js
@@ -11,14 +11,14 @@ plugin.init = function(params, callback) {
 	// We create two routes for every view. One API call, and the actual route itself.
 	// Just add the buildHeader middleware to your route and NodeBB will take care of everything for you.	
 	router.get('/admin/plugins/quickstart', hostMiddleware.admin.buildHeader, controllers.renderAdminPage);
-	router.get('/api/admin/plugins/quickstart', controllers.renderAdminPage);
+	router.get('/api/admin/plugins/thadTest', controllers.renderAdminPage);
 	
 	callback();
 };
 
 plugin.addAdminNavigation = function(header, callback) {
 	header.plugins.push({
-		route: '/plugins/quickstart',
+		route: '/plugins/thadTest',
 		icon: 'fa-tint',
 		name: 'Quickstart'
 	});
